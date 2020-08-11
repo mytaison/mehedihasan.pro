2c8560d3b466b84d2f40494772c6bc09f0ef0ae1
mytaison <mytaison@aol.com>
2020-08-12 02:43:25 +0600

    navpanel and event added for nav items

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var htmlTag = document.querySelector('html');
 var bodyTag = document.querySelector('body');
 var navTag = document.querySelector('nav');
+var asideTag = document.querySelector('aside');
 var footerTag = document.querySelector('footer');
 var date = new Date();
 var year = date.getFullYear();
@@ -15,15 +16,36 @@ function adjustDocumentHeightAndComponents(){
     document.querySelector('.container .content section').style.minheight = `calc(${windowInnerHeight} - (${navHeight} + ${footerHeight} - (${contentMargin}) * 2 )} )`;
     htmlTag.style.height = window.innerHeight + "px";
     bodyTag.style.height = window.innerHeight + "px";
+    asideTag.style.top = `${navHeight}`;
+    asideTag.style.height = `calc(${window.innerHeight}px - ${navHeight})`;
+}
+
+function toggleAsideMenu(){
+    let aside = document.querySelector('aside');
+    let visibility = window.getComputedStyle(aside).display;
+    if( visibility == "none" ){
+        aside.style.display = "flex";
+        document.querySelector('.menu').innerHTML = "<i class=\"fas fa-times\"></i>";
+    }else{
+        aside.style.display = "none";
+        document.querySelector('.menu').innerHTML = "<i class=\"fas fa-bars\"></i>";
+    }
+}
+
+function scrollToSection(){
+    alert("HI");
 }
 
 document.onreadystatechange = function () {
     if( document.readyState == "complete" ){
+        // adjusting heights
         adjustDocumentHeightAndComponents()
+        // detecting copyright year
         document.querySelector('.copyright').innerHTML = "&copy Mehedi " + year;
-        // const scroll = new SmoothScroll('a[href*="#"]', {
-        //     speed: 300
-        // });
+        // adding menu icon even
+        document.querySelector('.menu').addEventListener('click',toggleAsideMenu)
+        // adding event to menu items
+        document.querySelectorAll('.menu_items ul li').forEach( li => { li.addEventListener('click',scrollToSection) })
     }
 }
 
