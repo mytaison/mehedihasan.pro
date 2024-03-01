82557d52ffe22ab2398dfdbf9e68a590180b8c2b
Mehedi Hasan <mytaison@aol.com>
2024-03-01 15:45:56 +1100

    Theme Switching feature added.

---
+++ app/ui/components/shared/ThemeSwitcher.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import ThemeToggleSwitch from "./ThemeToggleSwitch/ThemeToggleSwitch";
+
+const ThemeSwitcher = ({ size }: { size?: number }) => {
+  const switchSize = size ? size : 16;
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return (
+      <Image
+        src={
+          "data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg== "
+        }
+        width={36}
+        height={36}
+        sizes="36x36"
+        alt="Loading Light/Dark Toggle"
+        priority={false}
+        title="Loading Light/Dark Toggle"
+      ></Image>
+    );
+  }
+  return (
+    <div>
+      <ThemeToggleSwitch
+        theme={resolvedTheme}
+        setTheme={setTheme}
+        size={switchSize}
+      />
+    </div>
+  );
+};
+
+export default ThemeSwitcher;
